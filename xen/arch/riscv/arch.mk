fddffb84d40a62eb22f123c90221d898c0e63d55
Oleksii Kurochko <oleksii.kurochko@gmail.com>
2022-12-13 10:08:05 +0200

    riscv: Fix build against binutils 2.38
    From version 2.38, binutils default to ISA spec version 20191213. This
    means that the csr read/write (csrr*/csrw*) instructions and fence.i
    instruction has separated from the `I` extension, become two standalone
    extensions: Zicsr and Zifencei. As the Xen uses those instruction,
    this causes the following build failure:
    
    ./arch/riscv/include/asm/system.h:68:
    Error: unrecognized opcode `csrs sstatus,2', extension `zicsr' required
    
    Signed-off-by: Oleksii Kurochko <oleksii.kurochko@gmail.com>

--- xen/arch/riscv/arch.mk
+++ xen/arch/riscv/arch.mk
@@ -11,6 +11,12 @@ CFLAGS-$(CONFIG_RISCV_64) += -mabi=lp64
 riscv-march-$(CONFIG_RISCV_ISA_RV64IMA) := rv64ima
 riscv-march-$(CONFIG_RISCV_ISA_C)       := $(riscv-march-y)c
 
+# Newer binutils versions default to ISA spec version 20191213 which moves some
+# instructions from the I extension to the Zicsr and Zifencei extensions.
+riscv-march-y := $(firstword $(subst -march=,, $(call cc-option,$(CC),\
+                                  -march=$(riscv-march-y)_zicsr_zifencei,\
+                                  -march=$(riscv-march-y))))
+
 # Note that -mcmodel=medany is used so that Xen can be mapped
 # into the upper half _or_ the lower half of the address space.
 # -mcmodel=medlow would force Xen into the lower half.
